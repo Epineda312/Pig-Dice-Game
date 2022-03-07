a3168a99a528cada577f0ef43bf07444260466d5
Airic <epineda773@protonmail.com>
2022-03-06 23:53:03 -0600

    Update script.js
    Refactor existing code. Add working reset button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//Selecting elements
+//Selecting Elements
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
@@ -13,15 +13,25 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting Conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  // Starting Conditions - Reset Button Functionality
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -36,7 +46,6 @@ btnRoll.addEventListener('click', function () {
   if (playing) {
     // 1. Generate a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
-    console.log(dice);
 
     // 2.Display the dice
     diceEl.classList.remove('hidden');
@@ -63,7 +72,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     // 2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       //Finish the game
       playing = false;
       document
@@ -80,8 +89,5 @@ btnHold.addEventListener('click', function () {
   }
 });
 
-//Reset Button Functionality
-btnNew.addEventListener('click', function () {
-  // 1. Remove winner class
-  // 2. Reset all player scores
-});
+//Add functionality to reset button
+btnNew.addEventListener('click', init);
